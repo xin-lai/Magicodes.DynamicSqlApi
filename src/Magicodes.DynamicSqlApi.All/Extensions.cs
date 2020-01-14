283ba9454a74a3ec65f0f82708c13058b09f1cfa
李文强 <codelove1314@live.cn>
2020-01-14 18:19:45 +0800

    重构Extensions

--- src/Magicodes.DynamicSqlApi.All/Extensions.cs
+++ src/Magicodes.DynamicSqlApi.All/Extensions.cs
@@ -5,13 +5,7 @@ using Magicodes.DynamicSqlApi.CsScript;
 using Magicodes.DynamicSqlApi.Dapper;
 using Magicodes.DynamicSqlApi.Sqlserver;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace Magicodes.DynamicSqlApi.All
 {
@@ -19,42 +13,12 @@ namespace Magicodes.DynamicSqlApi.All
     {
         public static void AddDynamicSqlApi(this IServiceCollection services, string connectionString = null, string sqlMapperFileName = "sqlMapper.xml")
         {
-            services.Configure<MvcOptions>(mvcOptions =>
-            {
-                AddConventions(mvcOptions);
-            });
-
-            
-            if (!string.IsNullOrWhiteSpace(connectionString))
-                services.AddTransient<IDbConnection>(p => new SqlConnection(connectionString));
-
-            services.AddTransient<ICodeBuilder, DefaultCodeBuilder>();
-            services.AddTransient<ICodeCompiler, CsScriptCodeCompiler>();
-            services.AddTransient<ISqlExecutor, DapperSqlExecutor>();
-            services.AddTransient<ITSqlParser, SqlServerTSqlParser>();
+            services.AddDynamicSqlApi<DefaultCodeBuilder, CsScriptCodeCompiler, DapperSqlExecutor, SqlServerTSqlParser>(connectionString, sqlMapperFileName);
         }
 
         public static void UseDynamicSqlApi(this IApplicationBuilder app)
         {
-            ConfigureAspNetCore(app.ApplicationServices);
-        }
-
-        private static void ConfigureAspNetCore(IServiceProvider serviceProvider)
-        {
-            //Add feature providers
-            var partManager = serviceProvider.GetService<ApplicationPartManager>();
-            
-            var codeBuilder = serviceProvider.GetService<ICodeBuilder>();
-            var code = codeBuilder.Build();
-
-            var codeCompiler = serviceProvider.GetService<ICodeCompiler>();
-            var assembly = codeCompiler.CompileCode(code);
-            partManager?.FeatureProviders?.Add(new GenericTypeControllerFeatureProvider(assembly));
-        }
-
-        private static void AddConventions(MvcOptions options)
-        {
-            options.Conventions.Add(new GenericControllerRouteConvention());
+            app.UseDynamicSqlApi();
         }
     }
 }
---
+++ src/Magicodes.DynamicSqlApi.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Magicodes.DynamicSqlApi.Core;
+using Magicodes.DynamicSqlApi.Core.CodeBuilder;
+using Magicodes.DynamicSqlApi.Core.DynamicApis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Magicodes.DynamicSqlApi.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="sqlMapperFileName"></param>
+        public static void AddDynamicSqlApi<TCodeBuilder, TCodeCompiler, TSqlExecutor, TTSqlParser>(this IServiceCollection services, string connectionString = null, string sqlMapperFileName = "sqlMapper.xml")
+            where TCodeBuilder : class, ICodeBuilder
+            where TCodeCompiler : class, ICodeCompiler
+            where TSqlExecutor : class, ISqlExecutor
+            where TTSqlParser : class, ITSqlParser
+        {
+            services.Configure<MvcOptions>(mvcOptions =>
+            {
+                AddConventions(mvcOptions);
+            });
+
+
+            if (!string.IsNullOrWhiteSpace(connectionString))
+                services.AddTransient<IDbConnection>(p => new SqlConnection(connectionString));
+
+            services.AddTransient<ICodeBuilder, TCodeBuilder>();
+            services.AddTransient<ICodeCompiler, TCodeCompiler>();
+            services.AddTransient<ISqlExecutor, TSqlExecutor>();
+            services.AddTransient<ITSqlParser, TTSqlParser>();
+        }
+
+        public static void UseDynamicSqlApi(this IApplicationBuilder app)
+        {
+            ConfigureAspNetCore(app.ApplicationServices);
+        }
+
+        public static void ConfigureAspNetCore(IServiceProvider serviceProvider)
+        {
+            var partManager = serviceProvider.GetService<ApplicationPartManager>();
+
+            var codeBuilder = serviceProvider.GetService<ICodeBuilder>();
+            var code = codeBuilder.Build();
+
+            var codeCompiler = serviceProvider.GetService<ICodeCompiler>();
+            var assembly = codeCompiler.CompileCode(code);
+            partManager?.FeatureProviders?.Add(new GenericTypeControllerFeatureProvider(assembly));
+        }
+
+        private static void AddConventions(MvcOptions options)
+        {
+            options.Conventions.Add(new GenericControllerRouteConvention());
+        }
+    }
+}
